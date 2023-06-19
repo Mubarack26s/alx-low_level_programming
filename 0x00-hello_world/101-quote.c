991bfa1ab8573cd2294fca3dc2d2dbc787879a54
Mubarack26s <malicktoure717@gmail.com>
2023-06-19 15:25:37 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
   */
 int main(void)
 {
-	fput(1, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n", stdout);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }

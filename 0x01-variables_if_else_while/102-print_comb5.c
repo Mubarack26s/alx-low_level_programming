bd442f1df82626b65168538a4bdf8b94284ef61b
Mubarack26s <malicktoure717@gmail.com>
2023-06-20 16:33:03 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
   *main - Entry point
-  *Description: print all possible combinatinos of a pair of 
+  *Description: print all possible combinatinos of a pair of
   *two digit numbers, without any repetition.
   *Return: always 0
   */
@@ -9,22 +9,20 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			if (i < j && i != j)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				continue;
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}

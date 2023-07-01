9f08e5c0a8d902b6b625d3448bfddec213293f00
Mubarack26s <malicktoure717@gmail.com>
2023-07-01 14:19:08 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,24 +10,40 @@
 
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+	char password[84];
+	int i = 0, sum = 0, diff_half1, diff_half2;
 
-	sum = 0;
-	srand(time(NULL));
+	srand(time(0));
 
-	for (i = 0; i < 100; i++)
+	while (sum < 2772)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		password[i] = 33 + rand() % 94;
+		sum += password[i++];
+	}
+	password[i] = '\0';
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+		diff_half1++;
+		for (i = 0; password[i]; i++)
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
+			if (password[i] >= (33 + diff_half1))
+			{
+				password[i] -= diff_half1;
+				break;
+			}
+		}
+		for (i = 0; password[i]; i++)
+		{
+			if (password[i] >= (33 + diff_half2))
+			{
+				password[i] -= diff_half2;
+				break;
+			}
 		}
 	}
+	printf("%s", password);
 	return (0);
 }
